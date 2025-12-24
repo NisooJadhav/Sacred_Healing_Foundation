9b09ad4f72f9ccf39717dc8c471ab0a55514068d
nisoojadhav <nisoojadhav@gmail.com>
2025-12-24 12:01:43 +0530

    removed gradient on hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -15,17 +15,6 @@ export default function Hero() {
         className="object-cover"
         sizes="100vw"
       />
-      {/* Overlay (optional – remove if not needed) */}
-      <div className="absolute inset-0 bg-black/10" />
-      {/* Explore More Indicator */}
-      <div className="absolute bottom-6 left-1/2 -translate-x-1/2 flex flex-col items-center text-white">
-        <span className="text-xs tracking-widest uppercase opacity-90">
-          Explore More
-        </span>
-
-        {/* Animated Arrow */}
-        <div className="mt-1 h-6 w-[1px] bg-white animate-bounce" />
-      </div>
     </section>
     </center>
   );
